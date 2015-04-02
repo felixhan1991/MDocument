8b837b41e928d0b05609c08878d4b323365b84f1
Hermawan Winata <shinwan2@gmail.com>
2015-04-02 07:16:51 +0000

    1. Fixed spacing and indentation. 2. Omitted second parameter "refres" on redirect so Firefox can be redirected.

--- application/modules/dashboard/controllers/dashboard.php
+++ application/modules/dashboard/controllers/dashboard.php
@@ -1,12 +1,18 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Dashboard extends Management_Controller {
-
-        public function __construct() {
-            parent::__construct();
-            $this->load->model(array('documents/document_m','documents/enroll_m','kategori/kategori_m',
-                'documents/mapk_m','drive/favorite_m','integra/akun_m'));
-        }
+class Dashboard extends Management_Controller
+{
+    public function __construct()
+	{
+		parent::__construct();
+		$this->load->model(array(
+			'documents/document_m',
+			'documents/enroll_m',
+			'kategori/kategori_m',
+			'documents/mapk_m',
+			'drive/favorite_m',
+			'integra/akun_m'));
+    }
         
 	public function index()
 	{
@@ -26,31 +32,25 @@ class Dashboard extends Management_Controller {
             
             $data['approveDoc']=$this->document_m->getJobApproveDocumentbyStatus($this->_idUserActive,4);
             
-
             $data['num_release']=count($this->document_m->getAllDocumentbyStatus(5));
             $data['num_shared']=count($this->document_m->getAllDocumentbyStatus(6));
 
             $data['num_fav']=count($this->favorite_m->getMyDocument($this->_idUserActive));
-     
            
             $this->display('main',$data);
 	}
-         public function logout()
+	
+    public function logout()
     {
-       
         $this->akun_m->removeTimeAccess($this->_idUserActive,null);
-        session_start();
+        
+		session_start();
         $this->session->unset_userdata('edoc_in');
         $this->session->sess_destroy();
         session_destroy();
-        
-        
-        
-        redirect('dashboard', 'refresh');
+		
+        redirect('dashboard');
     }
-        
-
-     
 }
 
 /* End of file welcome.php */
