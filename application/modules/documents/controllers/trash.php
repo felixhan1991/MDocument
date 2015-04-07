6d229949cecaec4735e660e690b49a57d9bc1c77
Hermawan Winata <shinwan2@gmail.com>
2015-04-07 10:20:20 +0000

    1. Reformat space and indentation. 2. Modified code to save tanggal_log and tanggal_dokumen as Y-m-d H:i:s to avoid error. 3. Modified wrong sql constraint in table dokumenxdepart that referencing id_dokumen.

--- application/modules/documents/controllers/trash.php
+++ application/modules/documents/controllers/trash.php
@@ -1,25 +1,23 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Trash extends Admin_Controller {
-
-        public function __construct() {
-            parent::__construct();
-            $this->load->helper('date','download','file');
-            $this->load->model(array('document_m','logging_m','enroll_m','file/file_m'));
-        }
+class Trash extends Admin_Controller 
+{
+	public function __construct() 
+	{
+		parent::__construct();
+		$this->load->helper('date','download','file');
+		$this->load->model(array('document_m','logging_m','enroll_m','file/file_m'));
+	}
         
 	public function index()
 	{    
-            $this->title="Arsip";
-            $data['documents'] = $this->document_m->getTrashDocument();
-            
-           
-            $data['logs'] = array();//get to database
-            $data['state']='trash';
-            $this->display('main_trash',$data);
+		$this->title="Arsip";
+		$data['documents'] = $this->document_m->getTrashDocument();
+		
+		$data['logs'] = array();//get to database
+		$data['state']='trash';
+		$this->display('main_trash',$data);
 	}        
-        
-   
 }
 
 /* End of file welcome.php */
--- application/modules/documents/models/logging_m.php
+++ application/modules/documents/models/logging_m.php
@@ -1,38 +1,40 @@
 <?php (defined('BASEPATH')) OR exit('No direct script access allowed');
 
-
-
-class Logging_m extends MY_Model  {
-    
-    public function __construct() {
+class Logging_m extends MY_Model  
+{    
+    public function __construct() 
+	{
         parent::__construct();
         $this->table='logging';
     }
     
-    public function make($data){
+    public function make($data)
+	{
         $data['keterangan_log']="Buat Dokumen berhasil!";
-        $data['tanggal_log']=date('d-m-Y G:i:s',now());
+        $data['tanggal_log']=date('Y-m-d H:i:s',now());
         $data['destination_status']=1;
         return $this->_insert($data);
     }
     
-    public function makeReview($data){
+    public function makeReview($data)
+	{
         $data['keterangan_log']="Buat Review berhasil!";
-        $data['tanggal_log']=date('d-m-Y G:i:s',now());
-        
+        $data['tanggal_log']=date('Y-m-d H:i:s',now());
         
         $data['destination_status']=-1;
         return $this->_insert($data);
     }
-    public function update($data){
-        
+	
+    public function update($data)
+	{    
         $data['keterangan_log']="Update finish!";
-        $data['tanggal_log']=date('d-m-Y G:i:s',now());
+        $data['tanggal_log']=date('Y-m-d H:i:s',now());
         
         return $this->_insert($data);
     }
     
-    public function sendTo($data){
+    public function sendTo($data)
+	{
         $query = "select * from status where id_status=?";
         $res = $this->_select($query,array($data['destination_status']));
         $destinasi="";
@@ -41,13 +43,15 @@ class Logging_m extends MY_Model  {
             $destinasi=$res[0]->nama_status;
         }
         $data['keterangan_log']="Ubah Status '".$destinasi."' berhasil!";
-        $data['tanggal_log']=date('d-m-Y G:i:s',now());
+        $data['tanggal_log']=date('Y-m-d H:i:s',now());
         return $this->_insert($data);
     }
     
-    public function upload($data){
+    public function upload($data)
+	{
         $data['keterangan_log']="Upload berhasil!";
-        $data['tanggal_log']=date('d-m-Y G:i:s',now());
+        $data['tanggal_log']=date('Y-m-d H:i:s',now());
+		
         return $this->_insert($data);
     }
     
@@ -61,13 +65,9 @@ class Logging_m extends MY_Model  {
         };
         return false;
     }
-    
-    
-    public function checkDoubleName($data) {
+	
+    public function checkDoubleName($data)
+	{
         
     }   
-    
-    
-    
-    
 }
